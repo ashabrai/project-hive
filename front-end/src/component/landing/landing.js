a1b828e964ca82f017b0f02b50a1bb9de8d69094
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-10-31 19:08:24 -0700

    progress, login still not working

---
+++ front-end/src/component/landing/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  render(){
+    console.log('Landing');
+    return (
+      <p> Landing </p>
+    );
+  }
+}
+
+export default Landing;
